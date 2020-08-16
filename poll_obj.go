bf9f95aee640cea4a3c9378a8a22e455966b56ec
sjustice <rseanjustice@gmail.com>
2020-08-16 13:59:00 -0700

    naming for poll objects

--- poll_obj.go
+++ poll_obj.go
@@ -16,17 +16,17 @@ const (
 	PollFieldVotingStatus PollField = "voting_status"
 )
 
-// Poll included in a Tweet is not a primary object on any endpoint
-type Poll struct {
-	ID              string       `json:"id"`
-	Options         []PollOption `json:"options"`
-	DurationMinutes int          `json:"duration_minutes"`
-	EndDateTime     string       `json:"end_datetime"`
-	VotingStatus    string       `json:"voting_status"`
+// PollObj included in a Tweet is not a primary object on any endpoint
+type PollObj struct {
+	ID              string          `json:"id"`
+	Options         []PollOptionObj `json:"options"`
+	DurationMinutes int             `json:"duration_minutes"`
+	EndDateTime     string          `json:"end_datetime"`
+	VotingStatus    string          `json:"voting_status"`
 }
 
-// PollOption contains objects describing each choice in the referenced poll.
-type PollOption struct {
+// PollOptionObj contains objects describing each choice in the referenced poll.
+type PollOptionObj struct {
 	Position int    `json:"position"`
 	Label    string `json:"label"`
 	Votes    int    `json:"votes"`
