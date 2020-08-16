312b6d5917ccc4009fabd0725d469c552d1b2c09
sjustice <rseanjustice@gmail.com>
2020-08-16 14:00:32 -0700

    naming for tweet objects

--- tweet_obj.go
+++ tweet_obj.go
@@ -44,62 +44,62 @@ const (
 	TweetFieldWithHeld TweetField = "withheld"
 )
 
-// Tweet is the primary object on the tweets endpoints
-type Tweet struct {
-	ID                 string                   `json:"id"`
-	Text               string                   `json:"text"`
-	Attachments        TweetAttachments         `json:"attachments"`
-	AuthorID           string                   `json:"author_id"`
-	ContextAnnotations []TweetContextAnnotation `json:"context_annotations"`
-	ConversationID     string                   `json:"conversation_id"`
-	CreatedAt          string                   `json:"created_at"`
-	Entities           EntitiesObj              `json:"entities"`
-	Geo                TweetGeo                 `json:"geo"`
-	InReplyToUserID    string                   `json:"in_reply_to_user_id"`
-	Language           string                   `json:"lang"`
-	NonPublicMetrics   TweetMetrics             `json:"non_public_metrics"`
-	OrganicMetrics     TweetMetrics             `json:"organic_metrics"`
-	PossibySensitive   bool                     `json:"possiby_sensitive"`
-	PromotedMetrics    TweetMetrics             `json:"promoted_metrics"`
-	PublicMetrics      TweetMetrics             `json:"public_metrics"`
-	ReferencedTweets   []TweetReferencedTweet   `json:"referenced_tweets"`
-	Source             string                   `json:"source"`
-	WithHeld           WithHeldObj              `json:"withheld"`
+// TweetObj is the primary object on the tweets endpoints
+type TweetObj struct {
+	ID                 string                      `json:"id"`
+	Text               string                      `json:"text"`
+	Attachments        TweetAttachmentsObj         `json:"attachments"`
+	AuthorID           string                      `json:"author_id"`
+	ContextAnnotations []TweetContextAnnotationObj `json:"context_annotations"`
+	ConversationID     string                      `json:"conversation_id"`
+	CreatedAt          string                      `json:"created_at"`
+	Entities           EntitiesObj                 `json:"entities"`
+	Geo                TweetGeoObj                 `json:"geo"`
+	InReplyToUserID    string                      `json:"in_reply_to_user_id"`
+	Language           string                      `json:"lang"`
+	NonPublicMetrics   TweetMetricsObj             `json:"non_public_metrics"`
+	OrganicMetrics     TweetMetricsObj             `json:"organic_metrics"`
+	PossibySensitive   bool                        `json:"possiby_sensitive"`
+	PromotedMetrics    TweetMetricsObj             `json:"promoted_metrics"`
+	PublicMetrics      TweetMetricsObj             `json:"public_metrics"`
+	ReferencedTweets   []TweetReferencedTweetObj   `json:"referenced_tweets"`
+	Source             string                      `json:"source"`
+	WithHeld           WithHeldObj                 `json:"withheld"`
 }
 
-// TweetAttachments specifics the type of attachment present in the tweet
-type TweetAttachments struct {
+// TweetAttachmentsObj specifics the type of attachment present in the tweet
+type TweetAttachmentsObj struct {
 	MediaKeys []string `json:"media_keys"`
 	PollIDs   []string `json:"poll_ids"`
 }
 
-// TweetContextAnnotation contain the context annotation
-type TweetContextAnnotation struct {
-	Domain TweetContext `json:"domain"`
-	Entity TweetContext `json:"entity"`
+// TweetContextAnnotationObj contain the context annotation
+type TweetContextAnnotationObj struct {
+	Domain TweetContextObj `json:"domain"`
+	Entity TweetContextObj `json:"entity"`
 }
 
-// TweetContext contains the elements which identify detailed information regarding the domain classificaiton based on the Tweet text
-type TweetContext struct {
+// TweetContextObj contains the elements which identify detailed information regarding the domain classificaiton based on the Tweet text
+type TweetContextObj struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
 
-// TweetGeo contains details about the location tagged by the user in this Tweet, if they specified one.
-type TweetGeo struct {
-	PlaceID     string              `json:"place_id"`
-	Coordinates TweetGeoCoordinates `json:"coordinates"`
+// TweetGeoObj contains details about the location tagged by the user in this Tweet, if they specified one.
+type TweetGeoObj struct {
+	PlaceID     string                 `json:"place_id"`
+	Coordinates TweetGeoCoordinatesObj `json:"coordinates"`
 }
 
-// TweetGeoCoordinates contains details about the coordinates of the location tagged by the user in this Tweet, if they specified one.
-type TweetGeoCoordinates struct {
+// TweetGeoCoordinatesObj contains details about the coordinates of the location tagged by the user in this Tweet, if they specified one.
+type TweetGeoCoordinatesObj struct {
 	Type        string    `json:"type"`
 	Coordinates []float64 `json:"coordinates"`
 }
 
-// TweetMetrics engagement metrics for the Tweet at the time of the request
-type TweetMetrics struct {
+// TweetMetricsObj engagement metrics for the Tweet at the time of the request
+type TweetMetricsObj struct {
 	Impressions       int `json:"impression_count"`
 	URLLinkClicks     int `json:"url_link_clicks"`
 	UserProfileClicks int `json:"user_profile_clicks"`
@@ -109,8 +109,8 @@ type TweetMetrics struct {
 	Quotes            int `json:"quote_count"`
 }
 
-// TweetReferencedTweet is a Tweet this Tweet refers to
-type TweetReferencedTweet struct {
+// TweetReferencedTweetObj is a Tweet this Tweet refers to
+type TweetReferencedTweetObj struct {
 	Type string `json:"type"`
 	ID   string `json:"id"`
 }
