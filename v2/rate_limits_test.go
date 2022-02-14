c7bdb740cefb80191a379e2c8fb252eac8da0429
sjustice <rseanjustice@gmail.com>
2022-02-13 18:22:41 -0700

    add unit test for rate limit helper

--- v2/rate_limits_test.go
+++ v2/rate_limits_test.go
@@ -1,6 +1,7 @@
 package twitter
 
 import (
+	"errors"
 	"net/http"
 	"reflect"
 	"testing"
@@ -49,6 +50,92 @@ func Test_rateFromHeader(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := rateFromHeader(tt.args.header); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("rateFromHeader() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRateLimitFromError(t *testing.T) {
+	type args struct {
+		err error
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  *RateLimit
+		want1 bool
+	}{
+		{
+			name: "error response",
+			args: args{
+				err: &ErrorResponse{
+					RateLimit: &RateLimit{
+						Limit:     15,
+						Remaining: 12,
+						Reset:     Epoch(1644461060),
+					},
+				},
+			},
+			want: &RateLimit{
+				Limit:     15,
+				Remaining: 12,
+				Reset:     Epoch(1644461060),
+			},
+			want1: true,
+		},
+		{
+			name: "http error",
+			args: args{
+				err: &HTTPError{
+					RateLimit: &RateLimit{
+						Limit:     15,
+						Remaining: 12,
+						Reset:     Epoch(1644461060),
+					},
+				},
+			},
+			want: &RateLimit{
+				Limit:     15,
+				Remaining: 12,
+				Reset:     Epoch(1644461060),
+			},
+			want1: true,
+		},
+		{
+			name: "response decode error",
+			args: args{
+				err: &ResponseDecodeError{
+					RateLimit: &RateLimit{
+						Limit:     15,
+						Remaining: 12,
+						Reset:     Epoch(1644461060),
+					},
+				},
+			},
+			want: &RateLimit{
+				Limit:     15,
+				Remaining: 12,
+				Reset:     Epoch(1644461060),
+			},
+			want1: true,
+		},
+		{
+			name: "error",
+			args: args{
+				err: errors.New("hit"),
+			},
+			want:  nil,
+			want1: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := RateLimitFromError(tt.args.err)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RateLimitFromError() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("RateLimitFromError() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
