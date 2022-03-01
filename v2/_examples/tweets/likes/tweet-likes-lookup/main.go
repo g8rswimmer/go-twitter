857203b8459146b3af7e0c9c134ffc41a691a3c6
sjustice <rseanjustice@gmail.com>
2022-02-28 18:55:29 -0700

    added the new examples

---
+++ v2/_examples/tweets/likes/tweet-likes-lookup/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	twitter "github.com/g8rswimmer/go-twitter/v2"
+)
+
+type authorize struct {
+	Token string
+}
+
+func (a authorize) Add(req *http.Request) {
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.Token))
+}
+
+/**
+	In order to run, the user will need to provide the bearer token and the list of tweet ids.
+**/
+func main() {
+	token := flag.String("token", "", "twitter API token")
+	id := flag.String("id", "", "user id")
+	flag.Parse()
+
+	client := &twitter.Client{
+		Authorizer: authorize{
+			Token: *token,
+		},
+		Client: http.DefaultClient,
+		Host:   "https://api.twitter.com",
+	}
+	opts := twitter.TweetLikesLookupOpts{
+		Expansions:  []twitter.Expansion{twitter.ExpansionPinnedTweetID},
+		TweetFields: []twitter.TweetField{twitter.TweetFieldCreatedAt, twitter.TweetFieldConversationID, twitter.TweetFieldAttachments},
+	}
+
+	fmt.Println("Callout to tweet like lookup callout")
+
+	tweetResponse, err := client.TweetLikesLookup(context.Background(), *id, opts)
+	if err != nil {
+		log.Panicf("tweet like lookup error: %v", err)
+	}
+
+	dictionaries := tweetResponse.Raw.UserDictionaries()
+
+	enc, err := json.MarshalIndent(dictionaries, "", "    ")
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(string(enc))
+}
