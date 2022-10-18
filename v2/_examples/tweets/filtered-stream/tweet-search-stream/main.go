4dde2b9e0265b53a0077538f656c4bddb2e13931
sjustice <rseanjustice@gmail.com>
2022-10-17 19:33:16 -0700

    updated the examples

--- v2/_examples/tweets/filtered-stream/tweet-search-stream/main.go
+++ v2/_examples/tweets/filtered-stream/tweet-search-stream/main.go
@@ -5,12 +5,13 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	twitter "github.com/g8rswimmer/go-twitter/v2"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	twitter "github.com/g8rswimmer/go-twitter/v2"
 )
 
 type authorize struct {
@@ -75,6 +76,14 @@ func main() {
 				outputFile.WriteString(fmt.Sprintf("system: %s\n\n", string(smb)))
 				outputFile.Sync()
 				fmt.Println("system")
+			case de := <-tweetStream.DisconnectionError():
+				ded, err := json.Marshal(de)
+				if err != nil {
+					fmt.Printf("error decoding disconnect message %v", err)
+				}
+				outputFile.WriteString(fmt.Sprintf("disconnect: %s\n\n", string(ded)))
+				outputFile.Sync()
+				fmt.Println("disconnect")
 			case strErr := <-tweetStream.Err():
 				outputFile.WriteString(fmt.Sprintf("error: %v\n\n", strErr))
 				outputFile.Sync()
